ed9942b14f174377a3b73308f4aef0b378f6743d
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-05-17 14:42:26 +0530

    user authentication done

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ const chats = require('./data')
 const cors = require('cors')
 const connectDB = require('./config/db');
 const colors = require('colors');
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 dotenv.config();
 connectDB()
 
 app.use(cors());
+
+app.use(express.json())  //to accept json data
 
 app.get('/',(req,res) => {
     res.send('API is RUNNING')
@@ -23,6 +26,8 @@ app.get('/api/chat/:id', (req,res) => {
     const singleChat = chats.find(chat => chat._id === req.params.id)
     res.send(singleChat)
 })
+
+app.use('/api/user',userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
