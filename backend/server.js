9d57b216866e8dcc46ed332f18bf611daa7fe2e9
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-07-17 21:26:47 +0530

    created search user api endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const connectDB = require('./config/db');
 const colors = require('colors');
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes')
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
@@ -29,6 +30,7 @@ app.get('/api/chat/:id', (req,res) => {
 })
 
 app.use('/api/user',userRoutes)
+app.use('/api/chat',chatRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
