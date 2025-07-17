600569a1d96bd46896771c11275ae87a5c687212
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-07-17 21:26:47 +0530

    created search user api endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { registerUser, authUser } = require('../controllers/userControllers');
+const { registerUser, authUser, allUsers } = require('../controllers/userControllers');
 
 const router = express.Router();
 
-router.post('/',registerUser);
+const {protect} = require("../middlewares/authMiddleware");
+
+router.route("/").post(registerUser).get(protect,allUsers);
 router.post('/login',authUser);
+
 
 module.exports = router;
