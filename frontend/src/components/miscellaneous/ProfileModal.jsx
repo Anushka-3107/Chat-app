4ff047cbcf444f4e0fae84cfc448132ab164ffe1
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-07-25 23:39:37 +0530

    completed account and logout functionality

---
+++ frontend/src/components/miscellaneous/ProfileModal.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import {
+  Button,
+  Center,
+  CloseButton,
+  Dialog,
+  For,
+  HStack,
+  IconButton,
+  Image,
+  Portal,
+  useDialog,
+  Text
+} from "@chakra-ui/react"
+
+
+const ProfileModal = ({user}) => {
+
+    const dialog = useDialog()
+
+    return (
+        <>
+          <Dialog.RootProvider
+            value={dialog}
+            placement="center"
+          >
+            <Dialog.Trigger
+             asChild 
+            textAlign="center"
+            >
+              <Button size="sm" variant="ghost">
+              Account
+             </Button>
+            </Dialog.Trigger>
+
+            <Portal>
+              <Dialog.Backdrop />
+              <Dialog.Positioner>
+                <Dialog.Content bg="white" borderRadius="lg" p={6} >
+                  <Dialog.Header>
+                    <Dialog.Title
+                     fontSize="40px"
+                     fontFamily="Work sans"
+                     textAlign="center"
+                     w="100%"
+                     color="black"
+                     >
+                     {user.name}
+                     </Dialog.Title>
+                  </Dialog.Header>
+                  <Dialog.Body >
+                    <Image
+                    borderRadius="full"
+                    boxSize="110px"
+                    src={user.img}
+                    alt={user.name}
+                    mx="auto"
+                    mt={4}
+                    mb={2}
+                    border="2px solid"
+                     borderColor="gray.200"
+                     shadow="md"
+                     textAlign="center"
+                     />
+                     <Text
+                     fontSize={{base:"20px", md:"20px" }}
+                     mt={4}
+                     fontFamily="Work sans"
+
+                     textAlign="center"
+                     color="black"
+                     >
+                      Email: {user.email}
+                     </Text>
+                  </Dialog.Body>
+
+                  <Dialog.Footer>
+                    <Dialog.ActionTrigger asChild>
+                      <Button variant="outline" color="black">Cancel</Button>
+                    </Dialog.ActionTrigger>
+                  </Dialog.Footer>
+                  <Dialog.CloseTrigger asChild>
+                    <CloseButton size="sm" variant="solid" color="black" />
+                  </Dialog.CloseTrigger>
+                </Dialog.Content>
+              </Dialog.Positioner>
+            </Portal>
+          </Dialog.RootProvider>
+       
+
+
+
+        </>
+  )
+}
+
+export default ProfileModal
+
+
--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -1,42 +1,36 @@
-// import { Box, Button, Tooltip } from "@chakra-ui/react";
-// // import { useState } from "react"
-// import { FaSearch } from "react-icons/fa";
-
-// const SideDrawer = () => {
-//   // const [search, setSearch] = useState("");
-//   // const [searchResult, setSearchResult] = useState([]);
-//   // const [loading, setLoading] = useState(false)
-//   // const [loadingChat,setLoadingChat] = useState();
-
-//   return (
-//     <Box>
-//       <Tooltip label="This is the tooltip content" hasArrow placement="bottom-end">
-//   <Button variant="outline" size="sm">
-//     <FaSearch />
-//   </Button>
-// </Tooltip>
-
-//     </Box>
-//   );
-// };
-
-// export default SideDrawer;
-
-import { Box, Button, Text , Menu, Portal } from "@chakra-ui/react";
+import { Box, Button, Text, Menu, Portal } from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 import { Tooltip } from "@/components/ui/tooltip";
 import { CiBellOn } from "react-icons/ci";
+import { BiChevronDown } from "react-icons/bi";
+import { Avatar, AvatarGroup } from "@chakra-ui/react";
+import { ChatState } from "@/Context/ChatProvider";
+import ProfileModal from "./ProfileModal";
+import { useNavigate } from "react-router-dom";
 
 const SideDrawer = () => {
+  // const [search, setSearch] = useState("");
+  // const [searchResult, setSearchResult] = useState([]);
+  // const [loading, setLoading] = useState(false)
+  // const [loadingChat,setLoadingChat] = useState();
+
+  const { user } = ChatState();
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/");
+  }
+
   return (
     <Box
-    display="flex"
-    justifyContent="space-between"
-    alignItems="center"
-    bg="white"
-    w="100%"
-    p="5px 10px 5px 10px"
-    borderWidth="2px"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="white"
+      w="100%"
+      p="5px 10px 5px 10px"
+      borderWidth="2px"
     >
       <Tooltip
         content="search users to chat"
@@ -45,37 +39,60 @@ const SideDrawer = () => {
       >
         <Button>
           <FaSearch />
-          <Text textStyle="sm" display={{base:"none", md:"flex"}} px={4}>
+          <Text textStyle="sm" display={{ base: "none", md: "flex" }} px={4}>
             Search User
           </Text>
         </Button>
       </Tooltip>
 
       <Text fontSize="2xl" fontFamily="Work Sans" color="blackAlpha.900">
-      Chat-tea-ing
+        Chat-tea-ing
       </Text>
 
-      <div>
-    <Menu.Root>
-      <Menu.Trigger asChild>
-        <Button size="sm" p={1}>
-         <CiBellOn />
-        </Button>
-      </Menu.Trigger>
-      <Portal>
-        <Menu.Positioner>
-          <Menu.Content>
-            <Menu.Item value="new-txt">New Text File</Menu.Item>
-            <Menu.Item value="new-file">New File...</Menu.Item>
-            <Menu.Item value="new-win">New Window</Menu.Item>
-            <Menu.Item value="open-file">Open File...</Menu.Item>
-            <Menu.Item value="export">Export</Menu.Item>
-          </Menu.Content>
-        </Menu.Positioner>
-      </Portal>
-    </Menu.Root>
-      </div>
+      <Box display="flex" alignItems="center" gap={2}>
+        <Menu.Root>
+          <Menu.Trigger asChild p={1}>
+            <Button fontSize="2xl" m={1}>
+              <CiBellOn />
+            </Button>
+          </Menu.Trigger>
 
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                <Menu.Item value="new-txt">New Text File</Menu.Item>
+                <Menu.Item value="new-file">New File...</Menu.Item>
+                <Menu.Item value="new-win">New Window</Menu.Item>
+                <Menu.Item value="open-file">Open File...</Menu.Item>
+                <Menu.Item value="export">Export</Menu.Item>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
+
+        <Menu.Root>
+          <Menu.Trigger rounded="full" focusRing="outside">
+            <Avatar.Root size="md">
+              <Avatar.Fallback name={user.name} />
+              <Avatar.Image src={user.img} />
+            </Avatar.Root>
+          </Menu.Trigger>
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                <Box textAlign="center">
+                  <ProfileModal user={user}>
+                    <Menu.Item value="account" justifyContent="center">
+                      Account
+                    </Menu.Item>
+                  </ProfileModal>
+                  <Menu.Item value="logout" justifyContent="center" onClick={logoutHandler}>Logout</Menu.Item>
+                </Box>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
+      </Box>
     </Box>
   );
 };
