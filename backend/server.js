51b31fb1e5c1ee668e55082123d28e8cded82a95
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-10-05 01:00:03 +0530

    removed bold

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ if(process.env.NODE_ENV === 'production'){
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT,()=>{
-    console.log(`SERVER STARTED ON PORT ${PORT}`.yellow.bold)
+    console.log(`SERVER STARTED ON PORT ${PORT}`)
 })
 
 const io = require("socket.io")(server,{
