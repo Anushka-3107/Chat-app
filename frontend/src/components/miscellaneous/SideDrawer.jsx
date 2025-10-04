3b510fbd777c8e1029f285d1911f6ac6879a4360
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-10-04 23:44:43 +0530

    made changes in json files and removed notification badge import

--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -24,8 +24,6 @@ import axios from "axios";
 import ChatLoading from "./ChatLoading";
 import UserListItem from "../UserAvatar/UserListItem";
 import { getSender } from "@/config/ChatLogics";
-import NotificationBadge from 'react-notification-badge';
-import {Effect} from 'react-notification-badge';
 
 
 const SideDrawer = () => {
