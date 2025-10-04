4319f611ff70f038bf5bf50a974d8a5e7c12c899
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-10-04 23:56:06 +0530

    changes in json

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom'
 import ChatProvider from './Context/ChatProvider'
 
 
-
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ChatProvider>
