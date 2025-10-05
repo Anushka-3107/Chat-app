e42d9d3ef3304b6e6727f3f207eb5f9ab7491e82
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-10-05 10:25:31 +0530

    made changes in server

--- backend/server.js
+++ backend/server.js
@@ -45,8 +45,8 @@ if(process.env.NODE_ENV === "production"){
 
     app.use(express.static(path.join(__dirname1, "frontend","dist")));
 
-    app.get('/{*any}',(req,res) => {
-        res.sendFile(path.resolve(__dirname1,"frontend","dist","index.html"));
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname1,"frontend","dist","index.html"));
 
     })
 }else{
